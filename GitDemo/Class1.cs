2e875929e4f48c0c7237f1ebf388cef0e1ff4aa8
Artem_tech <cerast123@gmail.com>
2025-02-12 13:56:08 +0300

    сделать невозможное

---
+++ GitDemo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitDemo
+{
+    class Class1
+    {
+        private static readonly Random _random = new Random();
+        private static readonly string[] _laughs = { "ха-ха-ха", "хи-хи-хи", "хо-хо-хо", "хе-хе-хе", "ху-ху-ху" };
+
+        public string GenerateLaugh()
+        {
+            int index = _random.Next(_laughs.Length);
+            return _laughs[index];
+        }
+    }
+}
