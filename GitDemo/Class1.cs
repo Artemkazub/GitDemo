9dd26cff190b4dd09dec9b74d5d65f18db113fcc
Артем_Лукашев <hronas125@gmail.com>
2025-02-12 13:55:08 +0300

    Написал свою функцию в Class1

--- GitDemo/Class1.cs
+++ GitDemo/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace GitDemo
 {
     class Class1
     {
+        public static int FunctionArtemL(int a)
+        {
+            return a * a;
+        }
     }
 }
