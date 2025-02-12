9707a528ea81e7b1799d46c77aca6735151f9b89
Artem_tech <cerast123@gmail.com>
2025-02-12 08:46:44 +0300

    Новое представление main

--- GitDemo/MainWindow.xaml.cs
+++ GitDemo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,10 @@ namespace GitDemo
         public MainWindow()
         {
             InitializeComponent();
-        }
+            double x = 0;
+            double y = 0;
+            int pricol = 22;
+            Debug.WriteLine("pricol, x, y");
+            }
     }
 }
